1eb33bfd1948cee6e8b4eb61033af0f4f709b639
Allyson Wesman <allysonwesman@gmail.com>
2017-12-29 05:56:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ function init() {
 
   function keyDownHandler(k) {
     console.log("key down");
-    let key = k.char;//parseInt(k.which || k.detail);
+    let key = k.code;//parseInt(k.which || k.detail);
     console.log(`key is: ${key}`);
 
     if (key === code[index]){
