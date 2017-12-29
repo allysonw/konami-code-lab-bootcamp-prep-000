4caa5388ed158e446a563fccf4e9cd8262e859fd
Allyson Wesman <allysonwesman@gmail.com>
2017-12-29 05:36:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ function init() {
 
     if (key === code[index]){
       index++;
-      console.log(`index is: ${index});
+      console.log(`index is: ${index}`);
 
       if (index === code.length) {
         alert("Hurray!");
